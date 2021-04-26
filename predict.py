56b9ff18d59019edec8d25339b7aa6918c662593
harry6-25 <harry@LEEdeMBP.lan>
2021-04-26 11:32:05 +0800

    Updated Candle Chart

--- predict.py
+++ predict.py
@@ -1,53 +1,50 @@
+# pip install streamlit fbprophet yfinance plotly
 import streamlit as st
-from datetime import datetime
-
+from datetime import date
 import yfinance as yf
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 import pandas as pd
 import numpy as np
-
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_style('whitegrid')
-plt.style.use("fivethirtyeight") 
-
-# For reading stock data from yahoo
-from pandas_datareader.data import DataReader
-
+plt.style.use("fivethirtyeight")
+col1, col2 = st.beta_columns(2)
 START = "2015-01-01"
-TODAY = datetime.today().strftime("%Y-%m-%d")
+TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title('Stock Forecast App')
-
-input_stock = st.text_input("Please Input the stock that you interested", "0005.HK")
-
-def load_stockdata(code):
-    df = DataReader(code, data_source='yahoo', start='2019-03-01', end=datetime.now())
-    return df
-
+st.write('Disclaimer: ')
+st.write('You expressly agree that the use of this app/website is at your sole risk.')
+selected_stock = st.text_input('Select dataset for prediction',"0005.HK")
 
 n_years = st.slider('Years of prediction:', 1, 4)
 period = n_years * 365
 
-data_load_state = st.text('Loading data...')
-data = load_stockdata(input_stock)
-data_load_state.text('Loading data... done!')
 
-st.subheader('Raw data')
-st.write(data)
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+data = load_data(selected_stock)
+
 
 # Plot raw data
-def plot_raw_data():
+def plot_raw_data(selected_stock):
+	fig = go.Figure(data=[go.Candlestick(x=data['Date'],
+                	open=data['Open'],
+                	high=data['High'],
+                	low=data['Low'],
+                	close=data['Close'])])
+	fig.layout.update(title_text=selected_stock, xaxis_rangeslider_visible=True)
+	st.plotly_chart(fig)
 
-	fig = go.Figure()
-	fig.add_trace(go.Scatter(x=data["Date"], y=data['Open'], name="stock_open"))
-	fig.add_trace(go.Scatter(x=data["Date"], y=data['Close'], name="stock_close"))
-	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
-	st.plotly_chart(fig) 
 
-plot_raw_data()
+plot_raw_data(selected_stock)
 
 # Predict forecast with Prophet.
 df_train = data[['Date','Close']]
@@ -57,15 +54,14 @@ m = Prophet()
 m.fit(df_train)
 future = m.make_future_dataframe(periods=period)
 forecast = m.predict(future)
-
-# Show and plot forecast
-st.subheader('Forecast data')
-st.write(forecast.tail())
     
 st.write(f'Forecast plot for {n_years} years')
 fig1 = plot_plotly(m, forecast)
-st.plotly_chart(fig1)
 
-st.write("Forecast components")
-fig2 = m.plot_components(forecast)
-st.write(fig2)
+st.plotly_chart(fig1,use_container_width = True)
+
+
+
+
+
+
