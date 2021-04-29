83eebdf5a32a7a5a9511d279f549f9512e967f8c
TOMLEUNGKS <lks1707@connect.hku.hk>
2021-04-29 14:00:32 +0800

    TL edited

--- predict.py
+++ predict.py
@@ -178,97 +178,112 @@ except:
 
 # # # # # # # # # # # # # # # # Prediction using LSTM# # # # # # # # # # # # # # # # # 
 st.header("Prediction with Long Short-term Memory (LSTM)")
-st.write(" ")
-# Create a new dataframe with only the 'Close column 
-LSTM_df = DataReader(selected_stock, data_source='yahoo', start='2019-03-01', end=datetime.now())
-LSTM_data = LSTM_df.filter(['Close'])
+try:
+	# Create a new dataframe with only the 'Close column 
+	LSTM_df = DataReader(selected_stock, data_source='yahoo', start='2019-03-01', end=datetime.now())
+	LSTM_data = LSTM_df.filter(['Close'])
 
-# Convert the dataframe to a numpy array
-LSTM_dataset = LSTM_data.values
-# Get the number of rows to train the model on
-training_data_len = int(np.ceil( len(LSTM_dataset) * .95 ))
+	# Convert the dataframe to a numpy array
+	LSTM_dataset = LSTM_data.values
+	# Get the number of rows to train the model on
+	training_data_len = int(np.ceil( len(LSTM_dataset) * .95 ))
 
-# Scale the data
-# from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler(feature_range=(0,1))
-scaled_data = scaler.fit_transform(LSTM_dataset)
+	# Scale the data
+	# from sklearn.preprocessing import MinMaxScaler
+	scaler = MinMaxScaler(feature_range=(0,1))
+	scaled_data = scaler.fit_transform(LSTM_dataset)
 
-# Create the training data set 
-# Create the scaled training data set
-train_data = scaled_data[0:int(training_data_len), :]
-# Split the data into x_train and y_train data sets
-x_train = []
-y_train = []
+	# Create the training data set 
+	# Create the scaled training data set
+	train_data = scaled_data[0:int(training_data_len), :]
+	# Split the data into x_train and y_train data sets
+	x_train = []
+	y_train = []
 
-print(len(train_data))
-for i in range(30, len(train_data)):
-    x_train.append(train_data[i-30:i, 0])
-    y_train.append(train_data[i, 0])
+	print(len(train_data))
+	for i in range(30, len(train_data)):
+		x_train.append(train_data[i-30:i, 0])
+		y_train.append(train_data[i, 0])
 
-    # if i<= 61:
-    #     print(x_train)
-    #     print(y_train)
-    #     print()
-      
-# Convert the x_train and y_train to numpy arrays 
-x_train, y_train = np.array(x_train), np.array(y_train)
+		# if i<= 61:
+		#     print(x_train)
+		#     print(y_train)
+		#     print()
+		
+	# Convert the x_train and y_train to numpy arrays 
+	x_train, y_train = np.array(x_train), np.array(y_train)
 
-# Reshape the data
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-# x_train.shape
+	# Reshape the data
+	x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+	# x_train.shape
 
-# from keras.models import Sequential
-# from keras.layers import Dense, LSTM
+	# from keras.models import Sequential
+	# from keras.layers import Dense, LSTM
 
-# Build the LSTM model
-model = Sequential()
-model.add(LSTM(128, return_sequences=True, input_shape= (x_train.shape[1], 1)))
-model.add(LSTM(64, return_sequences=False))
-model.add(Dense(25))
-model.add(Dense(1))
+	# Build the LSTM model
+	model = Sequential()
+	model.add(LSTM(128, return_sequences=True, input_shape= (x_train.shape[1], 1)))
+	model.add(LSTM(64, return_sequences=False))
+	model.add(Dense(25))
+	model.add(Dense(1))
 
-# Compile the model
-model.compile(optimizer='adam', loss='mean_squared_error')
+	# Compile the model
+	model.compile(optimizer='adam', loss='mean_squared_error')
 
-# Train the model
-model.fit(x_train, y_train, batch_size=1, epochs=1)
+	# Train the model
+	model.fit(x_train, y_train, batch_size=1, epochs=1)
 
-# Create the testing data set
-# Create a new array containing scaled values from index 1543 to 2002 
-test_data = scaled_data[training_data_len - 60: , :]
-# Create the data sets x_test and y_test
-x_test = []
-y_test = LSTM_dataset[training_data_len:, :]
-for i in range(60, len(test_data)):
-    x_test.append(test_data[i-60:i, 0])
-    
-# Convert the data to a numpy array
-x_test = np.array(x_test)
+	# Create the testing data set
+	# Create a new array containing scaled values from index 1543 to 2002 
+	test_data = scaled_data[training_data_len - 60: , :]
+	# Create the data sets x_test and y_test
+	x_test = []
+	y_test = LSTM_dataset[training_data_len:, :]
+	for i in range(60, len(test_data)):
+		x_test.append(test_data[i-60:i, 0])
+		
+	# Convert the data to a numpy array
+	x_test = np.array(x_test)
 
-# Reshape the data
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1 ))
+	# Reshape the data
+	x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1 ))
 
-# Get the models predicted price values 
-predictions = model.predict(x_test)
-predictions = scaler.inverse_transform(predictions)
+	# Get the models predicted price values 
+	predictions = model.predict(x_test)
+	predictions = scaler.inverse_transform(predictions)
 
-# Get the root mean squared error (RMSE)
-rmse = np.sqrt(np.mean(((predictions - y_test) ** 2)))
-# rmse
+	# Get the root mean squared error (RMSE)
+	rmse = np.sqrt(np.mean(((predictions - y_test) ** 2)))
+	# rmse
 
-# Plot the data
-train = data[:training_data_len]
-valid = data[training_data_len:]
-# valid['Predictions'] = predictions
-predictions = valid.iloc[:,1]
+	# Plot the data
+	train = data[:training_data_len]
+	valid = data[training_data_len:]
+	# valid['Predictions'] = predictions
+	predictions = valid.iloc[:,1]
 
 
-# Visualize the data
-plt.figure(figsize=(16,8))
-plt.title('Model')
-plt.xlabel('Date', fontsize=18)
-plt.ylabel('Close Price HKD ($)', fontsize=18)
-plt.plot(train['Close'])
-plt.plot(predictions)
-plt.legend(['Train', 'Predictions'], loc='lower right')
-plt.show()
+	# Visualize the data
+	plt.figure(figsize=(16,8))
+	plt.title('Model')
+	plt.xlabel('Date', fontsize=18)
+	plt.ylabel('Close Price HKD ($)', fontsize=18)
+	plt.plot(train['Close'])
+	plt.plot(predictions)
+	plt.legend(['Train', 'Predictions'], loc='lower right')
+	st.set_option('deprecation.showPyplotGlobalUse', False)
+	st.pyplot()
+	# plt.show()
+	# st.line_chart(mov_data[["mov_avg","Close"]])
+	# g = Prophet()
+	# g.fit(train['Close'])
+	# # future = m.make_future_dataframe(periods=period)
+	# p = Prophet().fit(predictions)
+	# st.write(f'Forecast of {selected_stock} using LSTM')
+	# fig2 = plot_plotly(g,p)
+	# st.plotly_chart(plt,use_container_width = True)
+
+
+
+except:
+	pass
