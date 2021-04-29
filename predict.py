654e895e0c879e16283e55468df6b194a8b293dd
TOMLEUNGKS <lks1707@connect.hku.hk>
2021-04-29 08:11:56 +0800

    TL modified

--- predict.py
+++ predict.py
@@ -11,6 +11,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import cufflinks as cf
+from datetime import datetime
+from pandas_datareader.data import DataReader
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+from sklearn.preprocessing import MinMaxScaler
+
 sns.set_style('whitegrid')
 plt.style.use("fivethirtyeight")
 col1, col2 = st.beta_columns(2)
@@ -158,3 +164,99 @@ st.bar_chart(df.forwardPE, 1000,250,use_container_width = True)
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
+# # # # # # # # # # # # # # # # Prediction using LSTM# # # # # # # # # # # # # # # # # 
+st.header("Prediction with Long Short-term Memory (LSTM)")
+
+# Create a new dataframe with only the 'Close column 
+LSTM_df = DataReader(selected_stock, data_source='yahoo', start='2019-03-01', end=datetime.now())
+LSTM_data = LSTM_df.filter(['Close'])
+
+# Convert the dataframe to a numpy array
+LSTM_dataset = LSTM_data.values
+# Get the number of rows to train the model on
+training_data_len = int(np.ceil( len(LSTM_dataset) * .95 ))
+
+# Scale the data
+# from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(LSTM_dataset)
+
+# Create the training data set 
+# Create the scaled training data set
+train_data = scaled_data[0:int(training_data_len), :]
+# Split the data into x_train and y_train data sets
+x_train = []
+y_train = []
+
+print(len(train_data))
+for i in range(30, len(train_data)):
+    x_train.append(train_data[i-30:i, 0])
+    y_train.append(train_data[i, 0])
+
+    # if i<= 61:
+    #     print(x_train)
+    #     print(y_train)
+    #     print()
+      
+# Convert the x_train and y_train to numpy arrays 
+x_train, y_train = np.array(x_train), np.array(y_train)
+
+# Reshape the data
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+# x_train.shape
+
+# from keras.models import Sequential
+# from keras.layers import Dense, LSTM
+
+# Build the LSTM model
+model = Sequential()
+model.add(LSTM(128, return_sequences=True, input_shape= (x_train.shape[1], 1)))
+model.add(LSTM(64, return_sequences=False))
+model.add(Dense(25))
+model.add(Dense(1))
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Train the model
+model.fit(x_train, y_train, batch_size=1, epochs=1)
+
+# Create the testing data set
+# Create a new array containing scaled values from index 1543 to 2002 
+test_data = scaled_data[training_data_len - 60: , :]
+# Create the data sets x_test and y_test
+x_test = []
+y_test = LSTM_dataset[training_data_len:, :]
+for i in range(60, len(test_data)):
+    x_test.append(test_data[i-60:i, 0])
+    
+# Convert the data to a numpy array
+x_test = np.array(x_test)
+
+# Reshape the data
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1 ))
+
+# Get the models predicted price values 
+predictions = model.predict(x_test)
+predictions = scaler.inverse_transform(predictions)
+
+# Get the root mean squared error (RMSE)
+rmse = np.sqrt(np.mean(((predictions - y_test) ** 2)))
+# rmse
+
+# Plot the data
+train = data[:training_data_len]
+valid = data[training_data_len:]
+# valid['Predictions'] = predictions
+predictions = valid.iloc[:,1]
+
+
+# Visualize the data
+plt.figure(figsize=(16,8))
+plt.title('Model')
+plt.xlabel('Date', fontsize=18)
+plt.ylabel('Close Price HKD ($)', fontsize=18)
+plt.plot(train['Close'])
+plt.plot(predictions)
+plt.legend(['Train', 'Predictions'], loc='lower right')
+plt.show()
